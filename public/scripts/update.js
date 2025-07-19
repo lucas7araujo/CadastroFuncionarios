706ec8157e5dce06020b3c0308d938182b9cfd09
Lucas Gabriel Adelino Araújo <lucasgabrielz2208@gmail.com>
2025-07-19 12:11:25 -0300

    Estrutura Inicial & Funcional JS | Update

--- public/scripts/update.js
+++ public/scripts/update.js
@@ -1,5 +1,8 @@
 const api = 'http://localhost:3000/funcionarios';
 
+let funcionarios = []; 
+let funcionarioSelecionado = null;
+
 async function carregarNomes() {
     const nomeSelecionado = document.getElementById('nome');
 
@@ -7,8 +10,7 @@ async function carregarNomes() {
         const resposta = await fetch(api);
         if (!resposta.ok) throw new Error('Erro ao buscar nomes');
 
-        const funcionarios = await resposta.json();
-
+        funcionarios = await resposta.json(); 
         const nomesUnicos = [...new Set(funcionarios.map(f => f.nome))].sort();
 
         nomesUnicos.forEach(nome => {
@@ -19,7 +21,54 @@ async function carregarNomes() {
         });
 
     } catch (error) {
-        console.error('Erro ao carregar nomes: ', error.message)
+        console.error('Erro ao carregar nomes: ', error.message);
     }
 }
+
+document.getElementById('nome').addEventListener('change', () => {
+    const nomeEscolhido = document.getElementById('nome').value;
+
+    funcionarioSelecionado = funcionarios.find(f => f.nome === nomeEscolhido); 
+    if (funcionarioSelecionado) {
+        document.getElementById('salario').value = funcionarioSelecionado.salario;
+        document.getElementById('cargo').value = funcionarioSelecionado.cargo;
+        document.getElementById('contrato').value = funcionarioSelecionado.contrato.toLowerCase();
+    }
+});
+
+async function atualizarFuncionario(event) {
+    event.preventDefault(); 
+
+    if (!funcionarioSelecionado) {
+        alert('Selecione um funcionário para atualizar');
+        return;
+    }
+
+    const id = funcionarioSelecionado.id;
+
+    const dadosAtualizados = {
+        ...funcionarioSelecionado,
+        salario: document.getElementById('salario').value,
+        cargo: document.getElementById('cargo').value,
+        contrato: document.getElementById('contrato').value.toUpperCase() 
+    };
+
+    try {
+        const resposta = await fetch(`${api}/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(dadosAtualizados)
+        });
+
+        if (!resposta.ok) throw new Error('Erro ao atualizar funcionário');
+
+        alert('Funcionário atualizado com sucesso!');
+    } catch (erro) {
+        alert('Erro ao atualizar: ' + erro.message);
+    }
+}
+
 document.addEventListener('DOMContentLoaded', carregarNomes);
+document.getElementById('botaoAtualizar').addEventListener('click', atualizarFuncionario);
